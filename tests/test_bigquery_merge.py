ef15894b98a3d57baee8b3c1b3705a98f3434cc5
nivanis91 <39068685+nivanis91@users.noreply.github.com>
2026-08-27 11:51:23 +0000

    integration test for backfilling with existing records

--- tests/test_bigquery_merge.py
+++ tests/test_bigquery_merge.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from google.cloud import bigquery
 import pytest
-from src.pipeline import merge_dataframe_to_bigquery
+from src.pipeline import merge_dataframe_to_bigquery, ingest_table_backfill
 
 
 TEST_TABLE_NAME = "ecommerce-data-platform-505412.test.orders"
@@ -16,6 +16,7 @@ def create_test_table(client):
         bigquery.SchemaField("order_id", "INT64"),
         bigquery.SchemaField("customer_id", "INT64"),
         bigquery.SchemaField("status", "STRING"),
+        bigquery.SchemaField("updated_at", "TIMESTAMP"),
     ]
 
     table = bigquery.Table(TEST_TABLE_NAME, schema=schema)
@@ -110,6 +111,68 @@ def test_merge_with_update_and_insert():
             (101, 1, 'completed'),
             (102, 2, 'completed'),
             (103, 3, 'on_delivery'),
+        ]
+
+    finally:
+        client.delete_table(TEST_TABLE_NAME, not_found_ok=True)
+
+
+def test_backfill_updates_existing_records():
+    client = bigquery.Client()
+
+    df = pd.DataFrame({
+        "order_id": [101, 102, 103, 104, 105],
+        "customer_id": [1, 2, 3, 4, 5],
+        "status": ["unknown", "pending", "pending", "completed", "completed"],
+        "updated_at": ["2026-08-01", "2026-08-02", "2026-08-04", "2026-08-05", "2026-08-06"]
+    })
+
+    df["updated_at"] = pd.to_datetime(
+                df["updated_at"],
+                utc=True
+    )
+
+    df_second = pd.DataFrame({
+        "order_id": [101, 102],
+        "customer_id": [1, 2],
+        "status": ["delivered", "on_delivery"]
+    })
+
+    create_test_table(client)
+
+    try:
+        # First ingestion
+        merge_dataframe_to_bigquery(
+            df,
+            TEST_TABLE_NAME,
+            ["order_id"]
+        )
+
+        # Backfill
+        ingest_table_backfill(
+            lambda conn, start_date_inclusive, end_date_exclusive: df_second,
+            TEST_TABLE_NAME,
+            ["order_id"],
+            "2026-08-01",
+            "2026-08-03",
+        )
+
+        query = f"""
+            SELECT order_id, customer_id, status
+            FROM `{TEST_TABLE_NAME}`
+            ORDER BY order_id
+        """
+
+        orders_list = list(client.query(query).result())
+
+        assert len(orders_list) == 5
+
+        assert [tuple(row) for row in orders_list] == [
+            (101, 1, 'delivered'),
+            (102, 2, 'on_delivery'),
+            (103, 3, 'pending'),
+            (104, 4, 'completed'),
+            (105, 5, 'completed'),
         ]
 
     finally:
